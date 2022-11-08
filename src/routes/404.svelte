bd21fe8900f9ef396af4e87dde354a7f1797ceef
Wenoc <eocrin.game@gmail.com>
2022-11-08 23:03:02 +0000

    404 9

---
+++ src/routes/404.svelte
@@ -0,0 +1,30 @@
+<svelte:head>
+  <title>404 - Page Not Found</title>
+</svelte:head>
+
+<div style="margin-top: 100px; text-align:center;">
+  <h2>404 - Page not found!</h2>
+  <p>
+    The page you are looking for was moved, removed, renamed, or might have
+    never existed.
+  </p>
+</div>
+
+<style>
+  h2,
+  p {
+    font-weight: 500;
+    margin: 0 auto;
+    color: white;
+  }
+
+  p {
+    margin: 1em auto;
+  }
+
+  @media (min-width: 480px) {
+    h2 {
+      font-size: 4em;
+    }
+  }
+</style>
