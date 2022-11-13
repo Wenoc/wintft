62e986c584bc3001e3f069aa3f402d2acc160c04
Wenoc <eocrin.game@gmail.com>
2022-11-13 08:36:10 +0000

    optimize

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -10,7 +10,7 @@
   <div class="navContainer">
     <div style="padding-top: 8px;">
       <a rel="external" sapper:prefetch href="/">
-        <img src="navbrand.png" alt="" height="48" />
+        <img src="navbrand.png" alt="home" height="48" />
       </a>
     </div>
     <div class="burger" id="burger" style="position: relative;">
