ff54990e14d79c3d76596453b87bfcb499e4f3b2
Wenoc <eocrin.game@gmail.com>
2022-11-13 08:26:24 +0000

    hover mobile

--- src/components/ItemDetails.svelte
+++ src/components/ItemDetails.svelte
@@ -35,72 +35,80 @@
 </script>
 
 <!-- svelte-ignore a11y-mouse-events-have-key-events -->
-<div
-  class="itemDetailContainer"
-  class:hidden={hidden == true && hoeverontext == false}
-  on:mouseover={hoeverontextf}
-  on:mouseleave={leaveontext}
->
-  <div class="up">
-    <div class="left">
-      <img
-        src="items/{name}.png"
-        alt="ArchangelsStaff"
-        height="50"
-        width="50"
-      />
-    </div>
-    <div class="right">
-      <p>{actualName}</p>
-      <div class="components">
-        {#if component1 != ""}
-          <img
-            src="./items/{component1}.png"
-            alt={component1}
-            height="28"
-            width="28"
-          />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            height="24"
-            width="24"
-            ><g data-name="Layer 2"
-              ><g data-name="plus"
-                ><rect
-                  width="24"
-                  height="24"
-                  opacity="0"
-                  transform="rotate(180 12 12)"
-                /><path
-                  fill="white"
-                  d="M19 11h-6V5a1 1 0 0 0-2 0v6H5a1 1 0 0 0 0 2h6v6a1 1 0 0 0 2 0v-6h6a1 1 0 0 0 0-2z"
-                /></g
-              ></g
-            ></svg
-          >
-          <img
-            src="./items/{component2}.png"
-            alt={component2}
-            height="28"
-            width="28"
-          />
-        {:else}
-          <span> Item Not Craftable </span>
-        {/if}
+<div class="absoluteContainer">
+  <div
+    class="itemDetailContainer"
+    class:hidden={hidden == true && hoeverontext == false}
+    on:mouseover={hoeverontextf}
+    on:mouseleave={leaveontext}
+  >
+    <div class="up">
+      <div class="left">
+        <img
+          src="items/{name}.png"
+          alt="ArchangelsStaff"
+          height="50"
+          width="50"
+        />
+      </div>
+      <div class="right">
+        <p>{actualName}</p>
+        <div class="components">
+          {#if component1 != ""}
+            <img
+              src="./items/{component1}.png"
+              alt={component1}
+              height="28"
+              width="28"
+            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              height="24"
+              width="24"
+              ><g data-name="Layer 2"
+                ><g data-name="plus"
+                  ><rect
+                    width="24"
+                    height="24"
+                    opacity="0"
+                    transform="rotate(180 12 12)"
+                  /><path
+                    fill="white"
+                    d="M19 11h-6V5a1 1 0 0 0-2 0v6H5a1 1 0 0 0 0 2h6v6a1 1 0 0 0 2 0v-6h6a1 1 0 0 0 0-2z"
+                  /></g
+                ></g
+              ></svg
+            >
+            <img
+              src="./items/{component2}.png"
+              alt={component2}
+              height="28"
+              width="28"
+            />
+          {:else}
+            <span> Item Not Craftable </span>
+          {/if}
+        </div>
       </div>
     </div>
-  </div>
-  <div class="down">
-    <span>
-      {description}
-    </span>
+    <div class="down">
+      <span>
+        {description}
+      </span>
+    </div>
   </div>
 </div>
 
 <style>
   .hidden {
     display: none;
+  }
+
+  .absoluteContainer {
+    position: absolute;
+    bottom: 30px;
+    z-index: 3;
   }
 
   .components {
@@ -111,9 +119,6 @@
     height: 100%;
   }
   .itemDetailContainer {
-    position: absolute;
-    bottom: 30px;
-    z-index: 3;
     width: 360px;
     height: auto;
     background-color: #22242f;
@@ -161,5 +166,11 @@
     font-weight: 400;
     color: white;
     line-height: 1.6;
+  }
+
+  @media (max-width: 700px) {
+    .absoluteContainer {
+      display: none;
+    }
   }
 </style>
